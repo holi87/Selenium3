d4b6e690ecaebe3567be0d8a6ee9291c70a4a358
Grzegorz Holak <holi87@gmail.com>
2017-09-06 16:00:18 +0200

    task 8

--- tests_main_page.py
+++ tests_main_page.py
@@ -1,7 +1,8 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.common.by import By
+
 
 
 @pytest.fixture
@@ -16,6 +17,23 @@ def wait(driver):
     return waiter
 
 
+def test_number_of_stickers(driver):
+    open_main_page(driver)
+    lista_produktow = driver.find_elements_by_css_selector("li[class='product column shadow hover-light']")
+    #  print(len(lista_produktow))
+    for element in lista_produktow:
+        tmp = element.find_elements_by_css_selector("div[class^='sticker']")
+        #  print(len(tmp))
+        assert len(tmp) == 1
+"""
+check amount of products and make loop in list of them to:
+check if every product has exactly one sticker
+
+"""
+
+def open_main_page(driver):
+    driver.get("http://localhost:8080/litecart")
+    wait(driver).until(ec.title_is("Online Store | My Store"))
 
 
 __author__ = "Grzegorz Holak"
