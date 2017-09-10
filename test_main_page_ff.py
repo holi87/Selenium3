0f6a599acb301e4a0d0128b534aa902add631ddf
Grzegorz Holak <holi87@gmail.com>
2017-09-10 15:15:57 +0200

    task 10

--- test_main_page_ff.py
+++ test_main_page_ff.py
@@ -29,19 +29,31 @@ def test_item_in_campaigns(driver):
     assert product_r_pr.value_of_css_property("color") == "rgb(119, 119, 119)", "regular price color is not gray #777"
     assert product_c_pr.value_of_css_property("color") == "rgb(204, 0, 0)", "campaign price color is not red #c00"
     assert product_r_pr.value_of_css_property("text-decoration") == "line-through", " regular price is not striked!"
-    assert product_c_pr.value_of_css_property("font-weight") == "900", "campaign price is not bolded!"
+    # as webdriver gives it as int not as string value like "bold", checked in css what is "normal" then everything
+    # with higher value is marked as bold
+    assert int(product_c_pr.value_of_css_property("font-weight")) > 400, "campaign price is not bolded!"
 
     product_list[0].click()
 
     # product page now
 
     pp_product_name = driver.find_element_by_css_selector("h1")
-    pp_regular_price = driver.find_element_by_css_selector("div.content .regular-price")
-    pp_campaign_price = driver.find_element_by_css_selector("div.content .campaign-price")
-    product_page_checklist = [pp_product_name.text, pp_regular_price.text, pp_campaign_price.text]
+    pp_reg_pr = driver.find_element_by_css_selector("div.content .regular-price")
+    pp_camp_pr = driver.find_element_by_css_selector("div.content .campaign-price")
+    product_page_checklist = [pp_product_name.text, pp_reg_pr.text, pp_camp_pr.text]
     for i in range(len(main_page_checklist)):
         assert main_page_checklist[i] == product_page_checklist[i], "It's not same product!"
 
+
+    # rgb(102,102,102) is still gray, but different than on main page, but it's really untestable to make what is grey
+    # and what is not exactly, cause it's impossible to make it in range of gray scale.
+
+    assert pp_reg_pr.value_of_css_property("color") == "rgb(102, 102, 102)", "regular price color is not gray #777"
+    assert pp_camp_pr.value_of_css_property("color") == "rgb(204, 0, 0)", "campaign price color is not red #c00"
+    assert pp_reg_pr.value_of_css_property("text-decoration") == "line-through", "regular price is not striked!"
+    # as webdriver gives it as int not as string value like "bold", checked in css what is "normal" then everything
+    # with higher value is marked as bold
+    assert int(pp_camp_pr.value_of_css_property("font-weight")) > 400, "campaign price is not bolded!"
 
 def open_main_page(driver):
     driver.get("http://localhost:8080/litecart")
